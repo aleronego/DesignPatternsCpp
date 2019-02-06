41b56eb469073421b988387d0cd1e7ffdc2bdea2
Don <aleronego@gmail.com>
2019-02-06 20:17:42 +0200

    Finalised shared ver of Chain

--- Behavioral/chain.cpp
+++ Behavioral/chain.cpp
@@ -16,20 +16,17 @@ class Chain
 {
     int skill;
     string name;
-    weak_ptr<Chain> next = weak_ptr<Chain>();
+    shared_ptr<Chain> next = shared_ptr<Chain>();
     void doWork(string s) { 
         cout << name << " can handle " << s << endl; 
     }
-    
 public:
-    //Chain(int s) : skill(s);
     Chain(int s, string n) : skill(s), name(n) {};
-    //~Chain() { if(next) delete next; }
 
     shared_ptr<Chain> setNext(shared_ptr<Chain> n)
     {
         next = move(n);
-        return move(next);
+        return next;
     }
 
     void process(shared_ptr<Action> action)
@@ -48,32 +45,19 @@ public:
  
 int main(int argc, char const *argv[])
 {
-    // Chain* handle = new Chain(3, "First 3");
-    // handle->setNext(new Chain(5, "Second 5"))
-    //   ->setNext(new Chain(9, "Third 9"));
-
-    // Action* arr[] {
-    //         new Action(2, "two"),
-    //         new Action(5, "five"),
-    //         new Action(11, "eleven"),
-    //         new Action(8, "eight") 
-    // };
-
-
-    shared_ptr<Chain> handle(new Chain(3, "First 3"));
+    shared_ptr<Chain> handle = make_shared<Chain>(3, "First 3");
     handle->setNext(make_shared<Chain>(5, "Second 5"))
-      ->setNext(make_shared<Chain>(9, "Third 9"));
+          ->setNext(make_shared<Chain>(9, "Third 9"));
 
-    Action* arr[] {
-            new Action(2, "two"),
-            new Action(5, "five"),
-            new Action(11, "eleven"),
-            new Action(8, "eight") 
+    shared_ptr<Action> arr[] {
+            make_shared<Action>(2, "two"),
+            make_shared<Action>(5, "five"),
+            make_shared<Action>(11, "eleven"),
+            make_shared<Action>(8, "eight") 
     };
+    
     for(auto&& act : arr)
-    {
         handle->process(act);
-        delete act;
-    }
+    
     return 0;
 }
